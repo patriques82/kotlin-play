fc798b5d9cec761b440f0ae459f2757fb18ae57b
Patrik Nygren <patriknygren82@gmail.com>
2019-01-23 14:46:34 +0100

    cleanup Dependency injection example

--- src/main/kotlin/K8reflection/7.DependencyInjection.kt
+++ src/main/kotlin/K8reflection/7.DependencyInjection.kt
@@ -1,7 +1,5 @@
 package K8reflection
 
-import org.reflections.Reflections
-import kotlin.reflect.KCallable
 import kotlin.reflect.KClass
 import kotlin.reflect.KType
 import kotlin.reflect.full.findAnnotation
@@ -9,12 +7,8 @@ import kotlin.reflect.full.memberFunctions
 import kotlin.reflect.full.starProjectedType
 
 // Inversion of Control is a principle in software engineering by which the control of objects is transferred to a
-// container or framework. Inversion of Control can be achieved through various mechanisms such as: Strategy design
-// pattern, Service Locator pattern, Factory pattern, and Dependency Injection (DI). In this case we explore Dependency
-// Injection
-
-@Target(AnnotationTarget.CLASS)
-annotation class Configuration
+// container or framework. In this case we explore Dependency Injection (DI) as mechanism to invert the control of
+// object creation.
 
 @Target(AnnotationTarget.FUNCTION)
 annotation class Bean
@@ -25,41 +19,54 @@ class DIContainer(config: Any) {
     val classMap: MutableMap<KType, Any?> = mutableMapOf()
 
     init {
-        val configClass = config::class
-        if (configClass.findAnnotation<Configuration>() == null) {
-            throw ConfigurationException("Class missing @Configuration annotation")
-        }
-        val functions = configClass.memberFunctions
+        val functions = config::class.memberFunctions
         val beanFunctions = functions.filter { it.findAnnotation<Bean>() != null }
+        if (beanFunctions.isEmpty()) {
+            throw ConfigurationException("Missing instance creation methods annotated with @Bean")
+        }
         beanFunctions.forEach {
             classMap[it.returnType] = it.call(config)
         }
     }
 
-    inline operator fun <reified T: Any> get(kClass: KClass<T>): T? {
-        return classMap[kClass.starProjectedType] as? T
-    }
+    inline operator fun <reified T: Any> get(kClass: KClass<T>): T? =
+        classMap[kClass.starProjectedType] as? T
 }
 
 interface SuperHero {
-    fun catchPhrase(): String
+    val name: String
+    val catchPhrase: String
 }
 
-class HumanTorch : SuperHero {
-    override fun catchPhrase()= "Flame on!"
+class HumanTorch: SuperHero {
+    override val name = "Human Torch"
+    override val catchPhrase = "Flame on!"
 }
 
-@Configuration
+interface Mission {
+    fun execute()
+}
+
+class SaveTheDay(val superHero: SuperHero): Mission {
+    override fun execute() {
+        println("${superHero.name} will save the day. ${superHero.catchPhrase}")
+    }
+}
+
 class AppConfig {
     @Bean
     fun superHero(): SuperHero {
         return HumanTorch()
     }
+
+    @Bean
+    fun mission(): Mission {
+        return SaveTheDay(superHero())
+    }
 }
 
 fun main(args: Array<String>) {
-    val config = AppConfig()
-    val diContainer = DIContainer(config)
-    val superHero = diContainer[SuperHero::class]
-    println(superHero?.catchPhrase()) // Flame on!
+    val diContainer = DIContainer(AppConfig())
+    val mission = diContainer[Mission::class]
+    mission?.execute() // Human Torch will save the day. Flame on!
 }
